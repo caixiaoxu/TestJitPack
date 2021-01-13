60cadeb918c1c8f14f6d94e7ad1d901d3c57b749
Lsy <1228357237@qq.com>
2021-01-13 10:28:08 +0800

    gitpack 测试module是否可以打包

--- app/src/main/java/com/lsy/myapplication/MainActivity.kt
+++ app/src/main/java/com/lsy/myapplication/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.lsy.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ContentUris
+import android.database.Cursor
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.appcompat.app.AppCompatActivity
 import com.lsy.l1.L1Test
 import com.lsy.l2.L2Test
+import com.lsy.m1.M1Test
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +18,60 @@ class MainActivity : AppCompatActivity() {
 
         L1Test.show()
         L2Test.show()
+        M1Test.show()
+
+//        val rxPermissions = RxPermissions(this)
+//        rxPermissions.request(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+//            .subscribe({ aBoolean ->
+//                if (aBoolean) {
+//
+//                } else {
+//                    Toast.makeText(this@MainActivity, "权限请求失败", Toast.LENGTH_LONG).show()
+//                }
+//            }, { it.printStackTrace() })
+//
+//        findViewById<TextView>(R.id.textview).setOnClickListener {
+//
+//            Thread {
+//                val file = File(
+//                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+//                    "WeiXin/wx_camera_1604142973828.mp4"
+//                )
+//                val file1 = File(
+//                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+//                    "WeiXin/wx_camera_1604142973828副本.mp4"
+//                )
+//
+//                val uri = getMediaUriFromPath(file.absolutePath)
+//
+//
+//                val path =
+//                    SiliCompressor.with(this).compressVideo(uri, file1.parent)
+//
+//                Log.e("视频压缩----------", "路径：$path")
+//            }.start()
+//        }
     }
+
+    /***
+     * 将指定路径的图片转uri
+     * @param context
+     * @param path ，指定图片(或文件)的路径
+     * @return
+     */
+//    fun getMediaUriFromPath(path: String): Uri? {
+//        val mediaUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+//        val cursor: Cursor? = contentResolver?.query(
+//            mediaUri, null, MediaStore.Video.Media.DISPLAY_NAME + "= ?",
+//            arrayOf(path.substring(path.lastIndexOf("/") + 1)), null
+//        )
+//        var uri: Uri? = null
+//        if (null != cursor && cursor.moveToFirst()) {
+//            uri = ContentUris.withAppendedId(
+//                mediaUri, cursor.getLong(cursor.getColumnIndex(MediaStore.Images.Media._ID))
+//            )
+//        }
+//        cursor?.close()
+//        return uri
+//    }
 }
---
+++ m1/src/main/java/com/lsy/m1/M1Test.kt
@@ -0,0 +1,19 @@
+package com.lsy.m1
+
+import android.util.Log
+import android.widget.Toast
+
+/**
+ * Title :
+ * Author: Lsy
+ * Date: 1/11/21 5:36 PM
+ * Version:
+ * Description:
+ * History:
+ * <author> <time> <version> <desc>
+ * 作者姓名 修改时间 版本号 描述
+ */
+object M1Test {
+
+    fun show(){ Log.e("m2--------","m2")}
+}
